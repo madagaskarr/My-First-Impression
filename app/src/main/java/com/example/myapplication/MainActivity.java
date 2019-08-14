69bb004a6e8ac33bfd7a1f882a3d958c7521173c
Tigran Ghazinyan <me@tigranes.io>
2019-08-14 00:01:34 -0700

    Added Button and RecyclerView to MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView personsRecyclerView;
+//    private PersonRecyclerViewAdapter personRecyclerViewAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        personsRecyclerView = findViewById(R.id.personsRecyclerView);
+        personsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        personsRecyclerView.setAdapter(personRecyclerViewAdapter);
     }
+
+    public void personsRecyclerView(View view) {
+
+    }
+
+
 }
